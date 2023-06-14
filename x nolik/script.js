6b46ecc7bc00cd920b2459369f052fcbb5af96f0
Aykhan Nasibli <130730594+aykhanzz@users.noreply.github.com>
2023-06-14 15:24:24 +0300

    neww

--- x nolik/script.js
+++ x nolik/script.js
@@ -2,7 +2,7 @@ var board = ['', '', '', '', '', '', '', '', ''];
 var currentPlayer = 'X';
 var gameOver = false;
 
-// Function to make a move on the board
+
 function makeMove(index) {
     if (!gameOver && board[index] === '') {
         board[index] = currentPlayer;
@@ -12,17 +12,16 @@ function makeMove(index) {
     }
 }
 
-// Function to toggle players
+
 function togglePlayer() {
     currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
 }
 
-// Function to check for a win
 function checkWin() {
     var winningCombinations = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
-        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
-        [0, 4, 8], [2, 4, 6] // Diagonals
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
     ];
 
     for (var i = 0; i < winningCombinations.length; i++) {
@@ -30,13 +29,26 @@ function checkWin() {
         if (board[a] !== '' && board[a] === board[b] && board[a] === board[c]) {
             gameOver = true;
             document.getElementById('board').style.pointerEvents = 'none';
-            alert(currentPlayer + ' wins!');
+            setTimeout(function () {
+                alert(currentPlayer + ' defeated!');
+                location.reload();
+            }, 100);
             break;
         }
     }
 
     if (!board.includes('') && !gameOver) {
         gameOver = true;
-        alert('It\'s a tie!');
+        setTimeout(function () {
+            alert('It\'s a tie!');
+            location.reload();
+        }, 100);
     }
 }
+
+
+
+let restartButton = document.getElementById("restartButton");
+restartButton.addEventListener("click", function () {
+    location.reload();
+});
